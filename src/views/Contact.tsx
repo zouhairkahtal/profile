84df42fa4f69e4575833423b2f3f68441b635925
zouhair kahtal <mizouhair4@gmail.com>
2025-12-07 22:58:45 +0100

    .

--- src/views/Contact.tsx
+++ src/views/Contact.tsx
@@ -2,17 +2,17 @@ import ClickSpark from "../components/ClickSpark";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Particles from '../components/Particles';
-
+import Particles from "../components/Particles";
 
 const contactSchema = z.object({
-  name: z.string().min(3, { message: "Name must be at least 3 characters long" }),
+  name: z
+    .string()
+    .min(3, { message: "Name must be at least 3 characters long" }),
   email: z.string().email({ message: "Invalid email address" }),
   message: z
     .string()
     .min(10, { message: "Message must be at least 10 characters long" }),
 });
-
 
 type ContactSchema = z.infer<typeof contactSchema>;
 
@@ -20,19 +20,16 @@ export function Contact() {
   const {
     register,
     handleSubmit,
-  
+
     formState: { errors },
-     reset
+    reset,
   } = useForm<ContactSchema>({
     resolver: zodResolver(contactSchema),
   });
 
   const onSubmit = (data: ContactSchema) => {
     console.log("Form Submitted:", data);
-      reset();
-    
-
-   
+    reset();
   };
 
   return (
@@ -43,39 +40,38 @@ export function Contact() {
         sparkRadius={50}
         sparkCount={6}
         duration={300}
-      >    <div
-  style={{
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100vw',
-    height: '100vh',
-    zIndex: 0,
-    pointerEvents: 'none'
-  }}
->
-  <Particles
-    particleColors={['#ef4444', '#b91c1c']}
-    particleCount={1000}
-    particleSpread={10}
-    speed={0.7}
-    particleBaseSize={100}
-    moveParticlesOnHover={false}
-    alphaParticles={true}
-    disableRotation={false}
-  />
-</div>
+      >
+        {" "}
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            zIndex: 0,
+            pointerEvents: "none",
+          }}
+        >
+          <Particles
+            particleColors={["#ef4444", "#b91c1c"]}
+            particleCount={1000}
+            particleSpread={10}
+            speed={0.7}
+            particleBaseSize={100}
+            moveParticlesOnHover={false}
+            alphaParticles={true}
+            disableRotation={false}
+          />
+        </div>
         <div className="bg-black w-full flex flex-col">
-   
           {/* nav */}
           <div className="border-b border-primary w-full h-20 flex items-center justify-center px-4">
             <div className="w-full h-full border-x border-primary max-w-[1889px]"></div>
-
           </div>
           {/* form */}
           <div className="border-b border-primary w-full flex items-center justify-center px-4">
             <div className="w-full h-[80vh] min-h-[800px] border-x border-primary max-w-[1889px] flex max-[600px]:flex-col items-center justify-center">
-          
               <form
                 onSubmit={handleSubmit(onSubmit)}
                 className="flex flex-col gap-6 w-[500px] max-[600px]:w-full p-6 border border-primary rounded-lg"
