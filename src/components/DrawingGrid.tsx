1b9bf76427eb00f3387bc96ff72590f10dfea3fb
zouhair kahtal <mizouhair4@gmail.com>
2026-02-03 19:18:08 +0100

    .

--- src/components/DrawingGrid.tsx
+++ src/components/DrawingGrid.tsx
@@ -5,7 +5,7 @@ export default function DrawingGrid() {
   return (
  
      <div className="w-full" style={{ height: '640px',width:"2000px", position: 'relative' }}>
-  <CircularGallery bend={0} textColor="#ffffff" borderRadius={0.05} scrollEase={0.02}
+  <CircularGallery bend={0} textColor="#ffffff" borderRadius={0.05} scrollEase={0.10}
   
 
 />
